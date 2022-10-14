e34b6d2b147356f6cfa944fe4aab369a0e194bd4
Svetlana Vasilevich <109768470+Svetlana-18@users.noreply.github.com>
2022-10-14 08:09:48 +0300

    нашла ошибку, исправила

--- Task2.py
+++ Task2.py
@@ -1,47 +1,39 @@
 # 3. Создайте программу для игры в "Крестики-нолики". Поле 3x3. Игрок - игрок, без бота.
 
-print()
 print("*" * 10, " Игра Крестики-нолики для двух игроков ", "*" * 10)
 
-board = list(range(1, 10))
-
+board = list(range(1,10))
 
 def draw_board(board):
-    print('-' * 20)
-    for i in range(3):
-        for k in range(3):
-            print(f"{board[k + i * 3]:^5}", end=" ")
-        print(f"\n{'-' * 20}")
-    print()
-
+   print("-" * 13)
+   for i in range(3):
+      print("|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|")
+      print("-" * 13)
 
 def take_input(player_token):
-    valid = False
-    while not valid:
-        player_answer = input("Куда поставим " + player_token+"?   ")
-        try:
-            player_answer = int(player_answer)
-        except:
-            print("Некорректный ввод. Вы уверены, что ввели число?")
-            continue
-        if player_answer >= 1 and player_answer <= 9:
-            if (str(board[player_answer-1]) not in "XO"):
-                board[player_answer-1] = player_token
-                valid = True
-            else:
-                print("Эта клетка уже занята!")
-        else:
-            print("Некорректный ввод. Введите число от 1 до 9.")
-
+   valid = False
+   while not valid:
+      player_answer = input("Куда поставим " + player_token+"? ")
+      try:
+         player_answer = int(player_answer)
+      except:
+         print("Некорректный ввод. Вы уверены, что ввели число?")
+         continue
+      if player_answer >= 1 and player_answer <= 9:
+         if(str(board[player_answer-1]) not in "XO"):
+            board[player_answer-1] = player_token
+            valid = True
+         else:
+            print("Эта клетка уже занята!")
+      else:
+        print("Некорректный ввод. Введите число от 1 до 9.")
 
 def check_win(board):
-    win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6),
-                 (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
-    for each in win_coord:
-        if board[each[0]] == board[each[1]] == board[each[2]]:
-            return board[each[0]]
-    return False
-
+   win_coord = ((0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6))
+   for each in win_coord:
+       if board[each[0]] == board[each[1]] == board[each[2]]:
+          return board[each[0]]
+   return False
 
 def main(board):
     counter = 0
@@ -49,24 +41,20 @@ def main(board):
     while not win:
         draw_board(board)
         if counter % 2 == 0:
-            take_input(f"{chr(10060)} ")
-
+           take_input("X")
         else:
-            take_input(f"{chr(11093)} ")
+           take_input("O")
         counter += 1
         if counter > 4:
-            tmp = check_win(board)
-            if tmp:
-                print()
-                print(tmp, f" Победа!{chr(127942)}{chr(127881)}!")
-                win = True
-                break
+           tmp = check_win(board)
+           if tmp:
+              print(tmp, f" Победа!{chr(127942)}{chr(127881)}!")
+              win = True
+              break
         if counter == 9:
             print("Ничья!")
             break
     draw_board(board)
-
-
 main(board)
 
 input("Нажмите Enter для выхода!")
